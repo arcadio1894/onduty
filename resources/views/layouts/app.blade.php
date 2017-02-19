33be5ffd6cf4ea4039375a8bfe2030bcd52de6bb
Jorge Gonzales Castillo <joryes1894@gmail.com>
2017-02-18 22:41:08 -0500

    Landing page onduty

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
     <nav class="top-nav">
         <div class="container">
             <div class="nav-wrapper">
-                <a href="#!" class="brand-logo">Logo</a>
+                <a href="{{ url('/') }}" class="brand-logo">Logo</a>
                 <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
                     @if (Auth::guest())
