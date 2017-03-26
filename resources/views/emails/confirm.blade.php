5f9ff03d7eb8b0f4171ddf6a6476822b854c53e3
Juan Carlos <juancagb.17@gmail.com>
2017-03-26 08:17:43 -0500

    Confirmation email change

--- resources/views/emails/confirm.blade.php
+++ resources/views/emails/confirm.blade.php
@@ -5,15 +5,15 @@
     <title>Document</title>
     <style>
         .logo {
-            width: 100px;
-            height: 53px;
+            max-width: 144px;
         }
     </style>
 </head>
 <body>
+    <img src="{{ asset('/images/logo/logo.png') }}" alt="Conciviles Logo" class="logo">
     <h3>Hola {{ $name }}, bienvenido a Conciviles</h3>
     <p>Por favor confirma tu correo electrónico.</p>
     <p>Para ello solo debes dar click en el siguiente enlace:</p>
-    <a href="{{ url('register/verify/' . $confirmation_code) }}">Click para confirmar tu correo electrónico</a>
+    <a href="{{ url('register/verify/' . $confirmation_code) }}">Clic aquí para confirmar tu correo electrónico</a>
 </body>
 </html>
