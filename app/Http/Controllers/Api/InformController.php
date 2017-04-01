ac6987b23fa5903ae366bf7b9bb08922ce57780d
Juan Carlos <juancagb.17@gmail.com>
2017-04-01 11:46:56 -0500

    new inform controller for api

---
+++ app/Http/Controllers/Api/InformController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Informe;
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class InformController extends Controller
+{
+    public function byUserLocation(Request $request)
+    {
+        // get the location where the user belongs
+        $user = User::find($request->input('user_id'));
+        $location_id = $user->location_id;
+
+        // and the informs in that location
+        $informs = Informe::where('location_id', $location_id)->get([
+            'user_id', 'from_date', 'to_date',
+            'created_at'
+        ]);
+
+        return $informs;
+    }
+}
