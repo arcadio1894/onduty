2a40771858b8f56aaa03e264609fc25ca711a43e
Juan Carlos <juancagb.17@gmail.com>
2017-08-17 20:39:45 -0500

    Iterate over the registration ids

--- app/Observers/ReportObserver.php
+++ app/Observers/ReportObserver.php
@@ -29,34 +29,39 @@ class ReportObserver
 
     public function sendNotification($location_id, $report_id)
     {
+
         $registrationIds = User::where('location_id', $location_id)->whereNotNull('fcm_token')
             ->pluck('fcm_token');
-
-        $data= [
-            'updated_entity' => 'report',
-            'updated_id' => $report_id
-        ];
-        $fields = [
-            'to'	=> $registrationIds,
-            'data'	=> $data
-        ];
 
         $headers = [
             'Authorization: key=' . env('FCM_ACCESS_KEY'),
             'Content-Type: application/json'
         ];
 
-        $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-        curl_setopt($ch,CURLOPT_POST, true);
-        curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($fields));
-        $result = curl_exec($ch);
-        curl_close($ch);
+        $data = [
+            'updated_entity' => 'report',
+            'updated_id' => $report_id
+        ];
 
-        Log::debug($result);
+        foreach ($registrationIds as $registrationId) {
+            $fields = [
+                'to'	=> $registrationId,
+                'data'	=> $data
+            ];
+
+            $ch = curl_init();
+            curl_setopt($ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+            curl_setopt($ch,CURLOPT_POST, true);
+            curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($fields));
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            Log::debug($result);
+        }
+
     }
 
 }
