5d25609531f0f341388a6abb78001ce806dfaeb7
Juan Carlos <juancagb.17@gmail.com>
2017-08-17 20:21:32 -0500

    Send FCM notification when a report is created/updated/deleted

--- app/Observers/ReportObserver.php
+++ app/Observers/ReportObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Report;
+use App\User;
 
 class ReportObserver
 {
@@ -12,6 +13,8 @@ class ReportObserver
         $inform = $report->inform;
         $inform->reports_updated_at = $report->updated_at;
         $inform->save();
+
+        $this->sendNotification($inform->location_id, $report->id);
     }
 
     public function deleted(Report $report)
@@ -19,6 +22,42 @@ class ReportObserver
         $inform = $report->inform;
         $inform->reports_updated_at = $report->deleted_at;
         $inform->save();
+
+        $this->sendNotification($inform->location_id, $report->id);
+    }
+
+    public function sendNotification($location_id, $report_id)
+    {
+        define('FCM_ACCESS_KEY', env('FCM_ACCESS_KEY'));
+        $registrationIds = User::where('location_id', $location_id)->whereNotNull('fcm_token')
+            ->pluck('fcm_token');
+
+        $data= [
+            'updated_entity' => 'report',
+            'updated_id' => $report_id
+        ];
+        $fields = array
+        (
+            'to'	=> $registrationIds,
+            'data'	=> $data
+        );
+
+        $headers = [
+            'Authorization: key=' . FCM_ACCESS_KEY,
+            'Content-Type: application/json'
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch,CURLOPT_POST, true);
+        curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($fields));
+        /*$result = */curl_exec($ch );
+        curl_close($ch);
+
+        // echo $result;
     }
 
 }
