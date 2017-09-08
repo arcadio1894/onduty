10bdeea4416e7a5fc0d70db6f2bd0969cbc82afb
Juan Carlos <juancagb.17@gmail.com>
2017-09-08 16:54:07 -0500

    Use Face File

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Report;
 use App\User;
-use Illuminate\Http\File;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
